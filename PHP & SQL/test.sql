46bc028ab40358791eb7bda56ec124112542619b
Faiza RABHI <faizarabhi8@gmail.com>
2022-12-16 13:13:11 +0100

    update

--- PHP & SQL/test.sql
+++ PHP & SQL/test.sql
@@ -1,6 +1,6 @@
-SELECT count(id) FROM employe ORDER BY (day(create_at))   ASC;
-SELECT * FROM employe WHERE (create_at = (2021-10-03));
-SELECT * FROM   employe WHERE email LIKE '[aeiouy]%[1-9]@%';
-SELECT LOWER(first_name) , LOWER(last_name) FROM  employe;
-ALTER TABLE employe RENAME COLUMN create_at TO date_creation;
+SELECT count(id) FROM employe GROUP BY (day(create_at))   ASC;
+SELECT * FROM employe WHERE  (create_at LIKE '2021-10-03%');
+SELECT * FROM   employe WHERE email LIKE '[^aeiouy]%[1-9]@%';
+UPDATE employe SET first_name = LOWER(first_name), last_name = LOWER(last_name) ;
+
 
