1bf88bcf336795d9e3aac58a9602d29b79796fd0
Faiza RABHI <faizarabhi8@gmail.com>
2022-12-16 09:07:58 +0100

    feat: update function

--- PHP & SQL/test.php
+++ PHP & SQL/test.php
@@ -3,13 +3,13 @@ function devOps() {
     
     for($i=1;$i<=100;$i++ ){
         if(($i%3 == 0) && ($i%5== 0)){
-            echo "Dev "."</br>";
+            echo "DevOps"."</br>";
         }
         elseif($i%3== 0){
             echo "Ops"."</br>";
         }
         elseif($i%5== 0){
-            echo "DevOps "."</br>";
+            echo "Dev"."</br>";
         }
         else {
             echo $i . "</br>";
@@ -19,16 +19,20 @@ function devOps() {
 }
 devOps();
 
-function isPrime($x){
-    $c=0;
-    for($i=0; $i<$x;$i++){
-        
-        if($x%$i==0){
-            $c++;
-        }
-
-    }
-    return $c == 2 ? true : false;
-}
-echo isPrime(73) ? 'Prime' : 'Composite';
+ function isPrime($x){
+     $c=0;
+     for($i=1; $i<$x;$i++){
+         if($x % $i ==0){
+             $c++;
+         }
+     }
+     return $c == 2 ? true : false;
+ }
+ echo isPrime(73) ? 'Prime' : 'Composite';
 ?>
+
+2022-12-15 23:17:54:513 +0100 - [localhost]: Saving to Cloud: Started
+2022-12-15 23:17:54:532 +0100 - [localhost]: Compressing document root folder…: Done
+2022-12-15 23:17:54:538 +0100 - [localhost]: Creating archive…: Done
+2022-12-15 23:17:54:540 +0100 - [localhost]: Moving data to desktop…: Done
+2022-12-15 23:17:54:540 +0100 - [localhost]: Saving to Cloud: Finished
--- PHP & SQL/test.sql
+++ PHP & SQL/test.sql
@@ -1,6 +1,6 @@
 SELECT count(id) FROM employe ORDER BY (day(create_at))   ASC;
 SELECT * FROM employe WHERE (create_at = (2021-10-03));
 SELECT * FROM   employe WHERE email LIKE '[aeiouy]%[1-9]@%';
-
 SELECT LOWER(first_name) , LOWER(last_name) FROM  employe;
+ALTER TABLE employe RENAME COLUMN create_at TO date_creation;
 
